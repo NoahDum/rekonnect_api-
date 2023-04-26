8a938bcab60ecedbcc9b407d9fdf6929226f7006
NoahDum <noahduminil6@gmail.com>
2023-04-26 10:44:07 +0200

    Creation du token de connexion  et insertion dans user

--- src/controller/login.php
+++ src/controller/login.php
@@ -12,8 +12,6 @@ class LoginController
     public function connect()
     {
 
-        session_start();
-
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
             $query = $this->model->db->prepare('SELECT * from rekonnect.users where name like :name');
@@ -24,45 +22,22 @@ class LoginController
 
             if (!empty($result) && password_verify($this->model->password, $result["password"])) {
 
-                $query = $this->model->db->prepare('SELECT rekonnect.users.id, rekonnect.users.name, rekonnect.users.email, rekonnect.users.adress, rekonnect.users.phone, rekonnect.users.adress_delivery, rekonnect.users.seller, rekonnect.users.buyer, rekonnect.users.repairer, rekonnect.users.avatar, rekonnect.users.wallet from rekonnect.users where name like :name');
+                $query = $this->model->db->prepare('SELECT rekonnect.users.id, rekonnect.users.name, rekonnect.users.email, rekonnect.users.adress, rekonnect.users.phone, rekonnect.users.adress_delivery, rekonnect.users.seller, rekonnect.users.buyer, rekonnect.users.repairer, rekonnect.users.avatar, rekonnect.users.wallet, rekonnect.users.token from rekonnect.users where name like :name');
                 $query->bindParam(':name', $result["name"]);
-
                 $query->execute();
                 $user = $query->fetch(PDO::FETCH_ASSOC);
-
+                if (!empty($user)) {
+                    $token = bin2hex(random_bytes(16)); // génère un token aléatoire
+                    $query = $this->model->db->prepare('UPDATE rekonnect.users SET token = :token WHERE id = :id');
+                    $query->bindParam(':token', $token);
+                    $query->bindParam(':id', $user['id']);
+                    $query->execute();
+                }
                 return $user;
             } else {
                 $error = "Nom d'utilisateur ou mot de passe incorrect";
                 return array("error" => $error);
             }
         }
-
-
-
-
-
-
-        // if (!empty($_POST)) {
-        //     $email = trim(strip_tags($_POST["email"]));
-        //     $password = trim(strip_tags($_POST["password"]));
-        // }
-
-        // $query = $this->model->db->prepare("SELECT * FROM  telefoot_auth.users where email like :email");
-        // $query->bindParam('email', $email);
-        // $query->execute();
-        // $result = $query->fetch(PDO::FETCH_ASSOC);
-
-
-        // if (!empty($result) && password_verify($password, $result["password"])) {
-        //     $_SESSION["user"] = [
-        //         "id" => $result["id"],
-        //         "email" => $result["email"],
-        //         "ip" => $_SERVER["REMOTE_ADDR"]
-        //     ];
-
-        //     header("Location: ./?page=myCompte");
-        // } else {
-        //     var_dump('false');
-        // }
     }
 }
